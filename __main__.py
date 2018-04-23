25076fa3f5fcae467adb479da28d11d036d9d2df
n0rfolk <n0rfolk.itsec@gmail.com>
2018-04-23 14:50:56 +0200

    Due to slow ST algorithm, trying new one

--- __main__.py
+++ __main__.py
@@ -1,13 +1,26 @@
 import sys, getopt, secrets, hashlib, sympy, math
 
 '''
-DSA - Digital Signature Algorithm
-@author = n0rfolk
-Based on publication FIPS 186-4
+    DSA - Digital Signature Algorithm
+    @author = n0rfolk
+    Based on publication FIPS 186-4
 '''
 
 # Chosen Hash() function SHA256, so outlen = 256
 outlen = 256
+
+# Fast modular exponentation
+def fast_modular_expn(base, exponent, modulus):
+    if modulus == 1:
+        return 0
+    result = 1
+    base = base % modulus
+    while (exponent > 0):
+        if (exponent % 2 == 1):
+            result = (result * base) % modulus
+        exponent = exponent >> 1
+        base = (base * base) % modulus
+    return result
 
 # Primality test
 def primality_test(number):
@@ -32,28 +45,28 @@ def st_random_prime(length, input_seed):
         old_counter = prime_gen_counter
         x = 0
         for i in range(iterations):
-            x += (hash(prime_seed + i) * pow(2, i * outlen))
-        prime_seed += iterations + 1
+            x = x + (hash(prime_seed + i) * pow(2, i * outlen))
+        prime_seed = prime_seed + iterations + 1
         x = pow(2, length - 1) + x % pow(2, length - 1)
         t = math.ceil(x/(2 * c_0))
 
         while True:
-            if 2 * t * c_0 + 1 > pow(2, length):
+            if (2 * t * c_0 + 1 > pow(2, length)):
                 t = math.ceil((pow(2, length - 1))/(2 * c_0))
             c = 2 * t * c_0 + 1
-            prime_gen_counter += 1
+            prime_gen_counter = prime_gen_counter + 1
             a = 0
             for i in range(iterations):
-                a += (hash(prime_seed + i) * pow(2, i * outlen))
-            prime_seed += iterations + 1
+                a = a + (hash(prime_seed + i) * pow(2, i * outlen))
+            prime_seed = prime_seed + iterations + 1
             a = 2 + (a % (c - 3))
             z = pow(a, 2 * t) % c
 
-            if (1 == math.gcd(z - 1, c)) and (1 == pow(z, c_0) % c):
+            if (1 == math.gcd(z - 1, c)) and (1 == fast_modular_expn(z, c_0, c)):
                 prime = c
                 return 'SUCCESS', prime, prime_seed, prime_gen_counter
 
-            if prime_gen_counter >= 4 * length + old_counter:
+            if (prime_gen_counter >= 4 * length + old_counter):
                 return 'FAILURE', 0, 0, 0
 
             t += 1
@@ -66,8 +79,8 @@ def st_random_prime(length, input_seed):
         c = pow(2, length - 1) + (c % pow(2, length - 1))
         c = (2 * math.floor(c/2) + 1)
 
-        prime_gen_counter += 1
-        prime_seed += 2
+        prime_gen_counter = prime_gen_counter + 1
+        prime_seed = prime_seed + 2
 
         if primality_test(c):
             prime = c
@@ -135,13 +148,53 @@ def constructive_prime_gen(L, N, firstseed):
         a = 2 + a % (p - 3)
         z = (pow(a, 2 * t * q) % p)
 
-        if (1 == math.gcd(z - 1, p)) and (1 == pow(z, p_0) % p):
+        if (1 == math.gcd(z - 1, p)) and (1 == fast_modular_expn(z, p_0, p)):
             return 'SUCCESS', p, q, pseed, qseed, pgen_counter, qgen_counter
 
         if pgen_counter > 4 * L + old_counter:
             return -1, -1
 
         t += 1
+
+def generation_pq_approved_hash(L, N, seedlen):
+    '''
+        Algorithm specification in Appendix A.1.1.2 in FIPS 186-4
+    '''
+
+    # Init
+    [status, p, q, domain_parameter_seed, counter] = ["INVALID", 0, 0, 0, 0]
+    # Acceptable values of (L, N)
+    acceptable_values = [(1024, 160), (2048, 224), (2048, 256), (3072, 256)]
+
+    # Check if (L, N) in acceptable list and if seedlen > N
+    if (L, N) not in acceptable_values:
+        return status, p, q, domain_parameter_seed, counter
+
+    if (seedlen < N):
+        return status, p, q, domain_parameter_seed, counter
+
+    # Now we can return to the generation process
+    n = math.ceil(L / outlen) - 1
+    b = L - 1 - (n * outlen)
+    domain_parameter_seed = secrets.randbits(seedlen)
+    U = hash(domain_parameter_seed) % pow(2, N - 1)
+    q = pow(2, N - 1) + U + 1 - (U % 2)
+    # TODO: Test primality of q with Appendix C.3
+    # TODO: If q not a prime GOTO step 5
+    offset = 1
+    for counter in range(4L):
+        for j in range(n):
+            Vj = hash((domain_parameter_seed + offset + j) % pow(2, seedlen))
+            # TODO: W parameter
+            X = W + pow(2, L - 1)
+            c = X % (2 * q)
+            p = X - (c - 1)
+            if p < pow(2, L - 1):
+                offset = offset + n + 1
+            # TODO: Test primality of p with Appendix C.3 if p is prime return p,q
+            # TODO: step 12: GOTO step 5
+
+    print (status)
 
 # DSA Signature Validation
 def dsa_signature_validation(l_bits, n_bits):
@@ -178,7 +231,8 @@ def main(argv):
     #print (l_bits, n_bits)
     #signature_validation = dsa_signature_validation(l_bits, n_bits)
     #print (signature_validation)
-    print(st_random_prime(37, 0))
+    #print(st_random_prime(256, 0))
+    print (generation_pq_approved_hash(1,1,1))
 
 # Standard main call
 if __name__ == '__main__':
